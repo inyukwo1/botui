b0369816c51c787d64db38a33cbb09e36dc8999d
Inhyuk Na <ina@dblab.postech.ac.kr>
2020-01-07 09:50:52 +0900

    gulpfile update for gulp version 4

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,3 @@
-
 var fs = require('fs'),
     gulp = require('gulp'),
     sass = require('gulp-sass'),
@@ -9,16 +8,17 @@ var fs = require('fs'),
     pkg = require('./package.json'),
     htmlclean = require('htmlclean'),
     replace = require('gulp-replace'),
+    log = require('fancy-log'),
     minify = require('gulp-clean-css');
 
-function escape (text) {
-  return text.replace(/'/g, "\\'").replace(/"/g, "\\\"");
+function escape(text) {
+    return text.replace(/'/g, "\\'").replace(/"/g, "\\\"");
 }
 
 function htmlTemplate() {
-  return replace('BOTUI_TEMPLATE', escape(
-    htmlclean(fs.readFileSync('./src/botui.html', 'utf8'))
-  ));
+    return replace('BOTUI_TEMPLATE', escape(
+        htmlclean(fs.readFileSync('./src/botui.html', 'utf8'))
+    ));
 }
 
 var comment = '/*\n' +
@@ -30,53 +30,57 @@ var comment = '/*\n' +
     ' * Released under the <%= pkg.license %> license.\n' +
     '*/\n\n';
 
-gulp.task('styles', function() {
-  gulp.src(['./src/styles/normal.scss',
-            './src/styles/botui.scss'])
-      .pipe(sass().on('error', sass.logError))
-      .pipe(minify())
-      .pipe(concat('botui.min.css'))
-      .pipe(banner(comment, {
-        pkg: pkg,
-        year: new Date().getFullYear()
-      }))
-      .pipe(gulp.dest('./build/'));
+gulp.task('styles', function(cb) {
+    gulp.src(['./src/styles/normal.scss',
+            './src/styles/botui.scss'
+        ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minify())
+        .pipe(concat('botui.min.css'))
+        .pipe(banner(comment, {
+            pkg: pkg,
+            year: new Date().getFullYear()
+        }))
+        .pipe(gulp.dest('./build/'));
+    cb();
 });
 
-gulp.task('themes', function() {
-  gulp.src('./src/styles/themes/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(minify())
-      .pipe(rename(function (path) {
-        path.basename = 'botui-theme-' + path.basename;
-      }))
-      .pipe(gulp.dest('./build/'));
+gulp.task('themes', function(cb) {
+    gulp.src('./src/styles/themes/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(minify())
+        .pipe(rename(function(path) {
+            path.basename = 'botui-theme-' + path.basename;
+        }))
+        .pipe(gulp.dest('./build/'));
+    cb();
 });
 
-gulp.task('scripts', function () {
-      gulp.src('./src/scripts/botui.js') // simply copy the original one
-      .pipe(htmlTemplate())
-      .pipe(banner(comment, {
-        pkg: pkg,
-        year: new Date().getFullYear()
-      }))
-      .pipe(gulp.dest('./build/'));
+gulp.task('scripts', function(cb) {
+    gulp.src('./src/scripts/botui.js') // simply copy the original one
+        .pipe(htmlTemplate())
+        .pipe(banner(comment, {
+            pkg: pkg,
+            year: new Date().getFullYear()
+        }))
+        .pipe(gulp.dest('./build/'));
 
-      gulp.src('./src/scripts/botui.js')  // minified version
-      .pipe(uglify())
-      .pipe(htmlTemplate())
-      .pipe(rename('botui.min.js'))
-      .pipe(banner(comment, {
-        pkg: pkg,
-        year: new Date().getFullYear()
-      }))
-      .pipe(gulp.dest('./build/'));
+    gulp.src('./src/scripts/botui.js') // minified version
+        .pipe(uglify())
+        .pipe(htmlTemplate())
+        .pipe(rename('botui.min.js'))
+        .pipe(banner(comment, {
+            pkg: pkg,
+            year: new Date().getFullYear()
+        }))
+        .pipe(gulp.dest('./build/'));
+    cb();
 });
 
-gulp.task('watch',function() {
-  gulp.watch('./src/styles/*.scss', ['styles']);
-  gulp.watch('./src/styles/themes/*.scss', ['themes']);
-  gulp.watch(['./src/scripts/botui.js', './src/botui.html'], ['scripts']);
+gulp.task('watch', function() {
+    gulp.watch('./src/styles/*.scss', gulp.series(['styles']));
+    gulp.watch('./src/styles/themes/*.scss', gulp.series(['themes']));
+    gulp.watch(['./src/scripts/botui.js', './src/botui.html'], gulp.series(['scripts']));
 });
 
-gulp.task('default', ['styles', 'scripts', 'themes']);
+gulp.task('default', gulp.series(['styles', 'scripts', 'themes']));
