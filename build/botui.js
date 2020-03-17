6309316457cf5275eba4a42f9d395cebf51ad21f
Inhyuk Na <ina@dblab.postech.ac.kr>
2020-03-18 01:20:22 +0900

    fix button callback

--- build/botui.js
+++ build/botui.js
@@ -350,9 +350,11 @@
         function _addMessageWithButtons(_msg, index) {
             const _index = _messageSetup(_msg, index);
             _msg.with_buttons = true;
-            _msg.callback = function() {
-                _msg.callback(index);
-            };
+            _msg.buttons.foreach(function(button) {
+                button.callback = function() {
+                    button.callback(index);
+                };
+            });
 
             return new Promise(function(resolve, reject) {
                 setTimeout(function() {
